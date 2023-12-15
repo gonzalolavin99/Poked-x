f3d7fbe1dcd7a2b11bd206bb7b17e78ce77767c1
gonzalolavin99 <gonzalolavin99@gmail.com>
2023-12-14 23:15:54 -0300

    1era llamada de API correcta

--- src/context/ContextAPI.jsx
+++ src/context/ContextAPI.jsx
@@ -1,26 +1,34 @@
 import React, { createContext } from "react";
 
-export const ContextAPI = createContext({pokeApi:{}});
+export const ContextAPI = createContext({ pokeApi: {} });
 
 const ContextProvider = ({ children }) => {
-    const url = "https://pokeapi.co/api/v2/pokemon/";
+  const url = "https://pokeapi.co/api/v2/pokemon/";
 
-    const pokeApi = async () => {
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            console.log(data);  
+  const pokeApi = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
 
-        } catch (err) {
-            console.log("Error al traer los datos de la API", err);
-        }
-    };
+      // Asegúrate de que data tenga la propiedad 'results'
+      if (data.results) {
+        console.log(data);
+        return data;
+      } else {
+        console.error('Los datos de la API no tienen la propiedad "results".', data);
+        return { results: [] }; // Retorna un objeto vacío si 'results' no está presente
+      }
+    } catch (err) {
+      console.log("Error al traer los datos de la API", err);
+      return { results: [] }; // Retorna un objeto vacío en caso de error
+    }
+  };
 
-    return (
-        <ContextAPI.Provider value={{ pokeApi }}>
-            {children}
-        </ContextAPI.Provider>
-    );
+  return (
+    <ContextAPI.Provider value={{ pokeApi }}>
+      {children}
+    </ContextAPI.Provider>
+  );
 };
 
 export default ContextProvider;
--- src/views/Pokemones.jsx
+++ src/views/Pokemones.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { ContextAPI } from '../context/ContextAPI';
 
 const Pokemones = () => {
-  return (
-    <div>Pokemones</div>
-  )
-}
+  const { pokeApi } = useContext(ContextAPI);
+  const [pokemons, setPokemons] = useState([]);
 
-export default Pokemones
+  useEffect(() => {
+    pokeApi()
+      .then(data => {
+        // Verifica que 'results' esté presente y no esté vacío
+        if (data.results && data.results.length > 0) {
+          setPokemons(data.results);
+        } else {
+          console.error('Los datos recibidos no tienen la propiedad "results" o está vacía.', data);
+        }
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos:', error);
+      });
+  }, [pokeApi]);
+
+  return (
+    <div>
+      <h2>Selecciona un pokémon</h2>
+      <select>
+        {pokemons.map(pokemon => (
+          <option key={pokemon.name} value={pokemon.name}>
+            {pokemon.name}
+          </option>
+        ))}
+      </select>
+      <button style={{backgroundColor:"black", color:"white"}}>Ver detalle</button>
+    </div>
+  );
+};
+
+export default Pokemones;
